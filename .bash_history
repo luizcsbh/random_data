defde06bc1a28322b7589274e02932b310080248
luizcsdev <luizcsdev@gmail.com>
2025-03-15 09:59:55 -0300

    Alterando arquivo

--- .bash_history
+++
@@ -1,3 +0,0 @@
-chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
-chmod -R 775 /var/www/storage /var/www/bootstrap/cache
-exit
---
+++ build-app.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Make sure this file has executable permissions, run `chmod +x build-app.sh`
+chmod +x build-app.sh
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
+# Exit the script if any command fails
+set -e
+
+# Build assets using NPM
+npm run build
+
+# Clear cache
+php artisan optimize:clear
