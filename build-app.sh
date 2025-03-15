0bcb1a25f0cea71c6a8d6ed5f28c92a9842291b6
luizcsdev <luizcsdev@gmail.com>
2025-03-15 10:19:51 -0300

    Alteracao

--- build-app.sh
+++ build-app.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Make sure this file has executable permissions, run `chmod +x build-app.sh`
-chmod +x build-app.sh
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 # Exit the script if any command fails
